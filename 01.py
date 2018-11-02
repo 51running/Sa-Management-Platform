47d9724a0fa68547409e2c8801af6e4129d72780
root <root@sadmin.(none)>
2018-11-02 16:49:16 +0800

    add test

---
+++ 01.py
@@ -0,0 +1,10 @@
+name = 'reboot'
+age = 20
+
+if name == 'reboot':
+    if age > 10:
+        print 'you are %s years old'%(age)
+    print 'condition is reboot'
+else:
+    print 'condition is false'
+print 'hello 321'
